2e763e6c58c150b944c8e827256774fcd5b48b1c
Corey <coreyjkelly@gmail.com>
2019-12-03 18:59:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import bibtexparser as bp
 
 def readFile(filename):
@@ -11,15 +12,19 @@ def writeFile(lib, filename):
         outFile.write(bp.dumps(lib))
 
 if __name__ == '__main__':
-    inFile = "Mendeley.bib"
-    bib = readFile(inFile)
+	if len(sys.argv) < 2:
+		print("Usage: python main.py filename.bib")
+	else:
+		inFile = sys.argv[1]
+		bib = readFile(inFile)
 
-    strip = ['file', 'abstract', 'keyword', 'eprint', 'archiveprefix', 'pmid', 'link', 'issn', 'isbn']
+		# add/remove fields depending on what you want
+		strip = ['file', 'abstract', 'keyword', 'eprint', 'archiveprefix', 'pmid', 'link', 'issn', 'isbn']
 
-    for k, v in bib.entries_dict.items():
-        for kk in strip:
-            if kk in v:
-                v.pop(kk)
+		for k, v in bib.entries_dict.items():
+			for kk in strip:
+				if kk in v:
+					v.pop(kk)
 
 
-    writeFile(bib, inFile.replace(".bib", "-out.bib"))
+		writeFile(bib, inFile.replace(".bib", "-out.bib"))
